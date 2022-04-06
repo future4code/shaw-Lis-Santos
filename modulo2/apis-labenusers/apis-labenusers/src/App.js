a930fa15da9c8732fa007192f190657e644ffe2a
Lis Ribeiro <lisribeiro197@gmail.com>
2022-04-06 19:04:21 -0300

    Integracoes de API - Atualizado

--- modulo2/apis-labenusers/apis-labenusers/src/App.js
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -1,91 +1,40 @@
 import React from 'react';
-import axios from 'axios';
-
-const headers = {
-  headers: {
-    Authorization: "lis-ribeiro-shaw"
-  }
-}
+import TelaCadastro from './Components/TelaCadastro';
+import TelaListaUsuarios from './TelaListaUsuarios';
 
 export default class App extends React.Component {
   state = {
-    usuarios: [],
-    inputName: "",
-    inputEmail: ""
-  }
-  componentDidMount() {
-    this.getAllUsers()
-  }
-  getAllUsers() {
-    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-    axios.get(url, headers)
-      .then((res) => {
-        console.log(res)
-        this.setState({
-          usuarios: res.data
-        })
-      })
-      .catch((error) => {
-        console.log(error.response.data.message)
-      })
-  }
-  postUsuario = () => {
-    console.log(this.state.inputName)
+    telaAtual: "cadastro"
   }
 
-  postEmail = () => {
-    console.log(this.state.inputEmail)
-  }
-
-  createUser = () => {
-    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/user"
-    const body = {
-      name: this.state.inputName,
-      email: this.state.inputEmail
+  escolhetela = () => {
+    switch (this.state.telaAtual) {
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irParaLista}/>
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro={this.irParaCadastro}/>
+      default:
+        return <div> Erro! Página não encontrada =(</div>
     }
-    axios.post(url, body, headers)
-      .then((res) => {
-        console.log(res.data)
-        this.getAllUsers()
-        this.setState({
-          inputName: ""
-        })
-      }).catch((error) => {
-        alert(error.response.data.message)
-      })
   }
-  onChangeName = (event) => {
-    this.setState({ inputName: event.target.value })
-  }
-  onChangeEmail = (event) => {
-    this.setState({ inputEmail: event.target.value })
-  }
-  render() {
-    const componentsUsers = this.state.usuarios.map((usuario) => {
-      return <li key={usuario.id}> {usuario.name}</li>
+
+  irParaCadastro = () => {
+    this.setState({
+      telaAtual: "cadastro"
     })
+  }
+
+  irParaLista = () => {
+    this.setState({
+      telaAtual: "lista"
+    })
+  }
+
+  render() {
     return (
       <div>
-        <form>
-        <label>
-          Nome:
-          <input
-            placeholder="Insira seu nome"
-            value={this.state.inputName} onChange={this.onChangeName} />
-        </label>
-        <label>
-          E-mail:
-          <input
-            placeholder="Insira seu e-mail"
-            value={this.state.inputEmail} onChange={this.onChangeEmail} />
-        </label>
-        <button onClick={this.postUsuario}>Enviar</button>
-        </form>
-        
-        <ul> {componentsUsers}</ul>
+        {this.escolhetela()}
       </div>
-
     )
   }
 }
