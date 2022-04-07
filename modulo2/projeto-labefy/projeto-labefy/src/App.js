29c84763fafbcb6e08a69f326d91f230d6327cc9
Lis Ribeiro <lisribeiro197@gmail.com>
2022-04-07 16:34:45 -0300

    Labefy

---
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    )
+  }
+}
