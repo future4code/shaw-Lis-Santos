0e5f4e7ffad30dfc581ecde39610ce68a1afb661
Lis Ribeiro <lisribeiro197@gmail.com>
2022-05-26 18:57:40 -0300

    aprofundamento-typescript

---
+++ modulo4/aprofundamento-typescript/src/exercicio5.ts
@@ -0,0 +1,3 @@
+// 5.a)
+
+/* Ao rodar o npm --init, algumas configurações vem inicialmente comentadas se comparadas com as do slide. Como, por exemplo: o sourceMap, outDir, rootDir, removeComments e noImplicitAny. */
