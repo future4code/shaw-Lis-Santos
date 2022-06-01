fa30e016ec4d0841a9e48b7ab9cb9bd2c4484365
Lis Ribeiro <99182969+lisfribeiro@users.noreply.github.com>
2022-05-31 22:31:34 -0300

    Delete exercicio5.ts

--- modulo4/aprofundamento-typescript/src/exercicio5.ts
+++
@@ -1,3 +0,0 @@
-// 5.a)
-
-/* Ao rodar o npm --init, algumas configurações vem inicialmente comentadas se comparadas com as do slide. Como, por exemplo: o sourceMap, outDir, rootDir, removeComments e noImplicitAny. */
