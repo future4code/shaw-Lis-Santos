ca3dd45e96866f761ee7f30aec4023915a091a74
Lis Ribeiro <99182969+lisfribeiro@users.noreply.github.com>
2022-05-31 22:32:40 -0300

    Delete exercicio2.ts

--- modulo4/intro-typescript/Exercício 2/src/exercicio2.ts
+++
@@ -1,5 +0,0 @@
-function imprimeTresCoresFavoritas(cor1:string, cor2:string, cor3:string): Array<string> {
-   return [cor1, cor2, cor3]
-}
-
-console.log(imprimeTresCoresFavoritas("azul", "preto", "azul"))
